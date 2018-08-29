62a6437d0c1d66a5b705888cc76f5bbbe66f84d5
andrespgd <42646798+andrespgd@users.noreply.github.com>
2018-08-29 10:22:18 -0500

    Update numpy_playground.py

--- numpy_playground.py
+++ numpy_playground.py
@@ -1,10 +1,29 @@
 import numpy as np
 
 
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(a)
+print(a.shape)
+print(a.dtype, '\n')
+
+a[:,2] = (a[:,2] - 100)/100
+print(a)
+print(a.shape)
+print(a.dtype, '\n')
+
+b = np.random.random((2, 3, 4))
+print(b)
+print(b.shape)
+print(b.dtype, '\n')
+
+
+b[:,:,0] = (b[:,:,0] - 100.0) / 100.0
+print(b)
+print(b.shape)
+print(b.dtype, '\n')
+
 
 # convert vector to column array
-
 feature_list = [0.5,0.3,0.0,5.0,2.0]
-
 X = np.vstack(feature_list).astype(np.float64)
 
