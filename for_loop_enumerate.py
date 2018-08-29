dbaf2da42bcefb9300fcb38074ccac6f73b5d9cd
andrespgd <42646798+andrespgd@users.noreply.github.com>
2018-08-29 10:47:33 -0500

    Create for_loop_enumerate.py

---
+++ for_loop_enumerate.py
@@ -0,0 +1,12 @@
+elements = ('cat', 'dog', 'hamster')
+for elem in elements:
+    print(elem)
+#cat
+#dog
+#hamster
+
+for count, elem in enumerate(elements):
+    print(count, elem)
+#0 cat
+#1 dog
+#2 hamster
