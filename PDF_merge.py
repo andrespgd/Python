08db23776eddef05d6b371e90fc3693ff23e00a2
andrespgd <42646798+andrespgd@users.noreply.github.com>
2018-09-02 08:50:19 -0500

    merges multiple PDFs

---
+++ PDF_merge.py
@@ -0,0 +1,16 @@
+import glob
+from PyPDF2 import PdfFileMerger
+ 
+def merger(output_path, input_paths):
+    pdf_merger = PdfFileMerger()
+ 
+    for path in input_paths:
+        pdf_merger.append(path)
+ 
+    with open(output_path, 'wb') as fileobj:
+        pdf_merger.write(fileobj)
+ 
+if __name__ == '__main__':
+    paths = glob.glob('201*.pdf')
+    paths.sort()
+    merger('pdf_merger2.pdf', paths)
