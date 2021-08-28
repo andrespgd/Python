72178187b7b265c8c1c7bc65491b50ddc162acbb
andrespgd <42646798+andrespgd@users.noreply.github.com>
2021-08-28 11:55:27 -0500

    Update class_dog.py

--- class_dog.py
+++ class_dog.py
@@ -1,6 +1,13 @@
+'''
+This code illustrates the need for __init__
+'''
+
+
+
 class Dog:
-    kind = 'canine' 
-    tricks = []
+    kind = 'canine'  # shared amongst ALL 'dogs' that will be created
+    tricks = []      # shared amongst ALL 'dogs' that will be created
+    print('hi')
     def __init__(self,name,age):
         self.name = name
         self.age = age   
@@ -12,18 +19,23 @@ class Dog:
         self.tricks.append(trick)
            
 yuyo = Dog('yuyo', 10)
-
-print(yuyo.getName())
-print(yuyo.getAge())
-
 yuyo.addTrick('runs_fast')
 yuyo.addTrick('rolls')
 
+print(yuyo.getName())
+print(yuyo.getAge())
 print(yuyo.tricks)
+print()
+
+matias = Dog('matias',20)
+print(yuyo.getName())
+print(yuyo.getAge())
+print(matias.tricks) # Matias has the same trics added to Yuyo
+print()
 
 
-print('\n\n VERY SIMPLE CLASS')
-print('--------------------')
+
+
 
 class Car:
     size  = 'large'
@@ -35,9 +47,7 @@ class Car:
     
 celica = Car
 celica.size = 'small'
-wb = 1.9
-mass = 800
-doors = 2
+celica.wb = 1.9
 celica.use = 'racing'
 
 print(celica.size)
@@ -45,3 +55,7 @@ print(celica.wb)
 print(celica.mass)
 print(celica.doors)
 print(celica.use)
+print()
+
+camry = Car
+print(camry.wb) # no longer 3.0
