fc52eb3d95c4880e578430c002e5572a8af8bc13
andrespgd <42646798+andrespgd@users.noreply.github.com>
2021-08-29 08:41:14 -0500

    Update class_dog.py

--- class_dog.py
+++ class_dog.py
@@ -3,14 +3,13 @@ This code illustrates the need for __init__
 '''
 
 
-
 class Dog:
-    kind = 'canine'  # shared amongst ALL 'dogs' that will be created
-    tricks = []      # shared amongst ALL 'dogs' that will be created
+    kind = 'canine'  # class variable
+    tricks = []      # class variable
     print('hi')
     def __init__(self,name,age):
-        self.name = name
-        self.age = age   
+        self.name = name # instance variable
+        self.age = age   # instance variable
     def getName(self):
         return self.name      
     def getAge(self):
@@ -33,29 +32,3 @@ print(yuyo.getAge())
 print(matias.tricks) # Matias has the same trics added to Yuyo
 print()
 
-
-
-
-
-class Car:
-    size  = 'large'
-    wb    = 3.0
-    mass  = 1000.0
-    doors = 4
-    use = ''
-    color = ''
-    
-celica = Car
-celica.size = 'small'
-celica.wb = 1.9
-celica.use = 'racing'
-
-print(celica.size)
-print(celica.wb)
-print(celica.mass)
-print(celica.doors)
-print(celica.use)
-print()
-
-camry = Car
-print(camry.wb) # no longer 3.0
