72f4cf129d4ce2c04b2836df5e37010597b3087f
andrespgd <42646798+andrespgd@users.noreply.github.com>
2022-04-29 11:14:54 -0500

    Update class_dog.py

--- class_dog.py
+++ class_dog.py
@@ -1,34 +1,58 @@
-'''
-This code illustrates the need for __init__
-'''
-
-
-class Dog:
-    kind = 'canine'  # class variable
-    tricks = []      # class variable
-    print('hi')
-    def __init__(self,name,age):
-        self.name = name # instance variable
-        self.age = age   # instance variable
-    def getName(self):
-        return self.name      
-    def getAge(self):
-        return self.age     
-    def addTrick(self,trick):
+class Dog():
+    # class variables
+    kind = 'canine'
+    extinction = False
+    def __init__(self, name, age, breed=None):
+        # attributes
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.color = None
+        self.tricks = []
+        self.vaccines = []
+    # methods
+    def get_name(self):
+        return self.name
+    def get_age(self):
+        return self.age
+    # behavior method
+    def add_trick(self, trick):
         self.tricks.append(trick)
-           
+        
+        
+print('Create Yuyo')
 yuyo = Dog('yuyo', 10)
-yuyo.addTrick('runs_fast')
-yuyo.addTrick('rolls')
-
-print(yuyo.getName())
-print(yuyo.getAge())
+print(vars(yuyo))
+yuyo.get_name()          
+yuyo.add_trick('runs_fast')
+yuyo.add_trick('rolls')
 print(yuyo.tricks)
+print(vars(yuyo))
+yuyo.vaccines.append('distemper')
+print(vars(yuyo))
 print()
 
-matias = Dog('matias',20)
-print(yuyo.getName())
-print(yuyo.getAge())
-print(matias.tricks) # Matias has the same trics added to Yuyo
+print('Create Matias')
+matias = Dog('matias', 5, 'Poodle')
+print(vars(matias))
+# create a new attribute for an instance
+matias.playful = True
+print(vars(matias))
 print()
 
+print('Change a class variable for an instance only')
+matias.extinction = True
+print(yuyo.extinction)
+print(matias.extinction)
+print()
+
+print('Change a class variable for ALL members of the class')
+Dog.kind = 'cat'
+print(yuyo.kind)
+print(matias.kind)
+
+print()
+print(vars(yuyo))
+
+print()
+print(vars(matias))
