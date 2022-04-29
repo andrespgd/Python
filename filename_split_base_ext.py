92be5092260f79bc1fc640448e1c197d7247ac01
andrespgd <42646798+andrespgd@users.noreply.github.com>
2022-04-29 16:06:42 -0500

    Update filename_split_base_ext.py

--- filename_split_base_ext.py
+++ filename_split_base_ext.py
@@ -12,3 +12,19 @@ print(base)
 # extension
 ext=os.path.splitext(fname)[1]
 print(ext)
+
+
+
+import pathlib
+
+file = r'C:\tmp\untitled0.py'
+file_pathlib = pathlib.Path(file)
+
+# folder path
+folder_names = file_pathlib.parent
+
+# basename
+file_without_extension = file_pathlib.stem
+
+# extension
+file_extension = file_pathlib.suffix
