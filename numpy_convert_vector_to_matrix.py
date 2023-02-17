5b7424100f6b8d0b34d226227d52c6b5ce67a220
andrespgd <42646798+andrespgd@users.noreply.github.com>
2023-02-17 10:06:36 -0600

    Update numpy_convert_vector_to_matrix.py

--- numpy_convert_vector_to_matrix.py
+++ numpy_convert_vector_to_matrix.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-a = np.random.rand(20,10) # Matrix
-b = np.random.rand(20)    # Vector
-c = np.random.rand(20)    # Vector
+a = np.random.rand(20,10) # Matrix (20,10)
+b = np.random.rand(20)    # Vector (20,)
+c = np.random.rand(20)    # Vector (20,)
 
-b2 = np.expand_dims(b, axis=1) # converts to Matrix
-c2 = np.expand_dims(c, axis=1) # converts to Matrix
+b2 = np.expand_dims(b, axis=1) # Vector --> Matrix (20, 1)
+c2 = np.expand_dims(c, axis=1) # Vector --> Matrix (20, 1)
 
-x1 = np.hstack((a,b2,c2))
+x1 = np.hstack((a,b2,c2)) # Matrix (20, 12) # Matrix (20,12)
 
-x2 = np.concatenate((a, b2, c2), axis=1)
+x2 = np.concatenate((a, b2, c2), axis=1) # Matrix (20, 12)
 
 # NOTE if using (a, b, c) => WON'T WORK!!! Mismatch!!
