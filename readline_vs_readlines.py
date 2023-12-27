b82f76cc498ab1dd3ce0ae9739cdac221ca97795
andrespgd <munoznajar@yahoo.com>
2023-12-27 12:21:58 -0600

    simplified filename

--- readline_vs_readlines.py
+++ readline_vs_readlines.py
@@ -1,18 +1,19 @@
 
+file_name = r'readline_vs_readlines.txt'
 
-with open('readline_vs_readlines.txt') as file:
+with open(file_name) as file:
     line1 = file.readline()
     line2 = file.readline()
     line3 = file.readline() 
     lines = file.read()
     
-with open('readline_vs_readlines.txt') as file:  
+with open(file_name) as file:  
     lines5 = file.readlines()
 
-with open('readline_vs_readlines.txt') as file:  
+with open(file_name) as file:  
     lines6 = file.read()
 
 import pathlib
-p = pathlib.Path('readline_vs_readlines.txt')
+p = pathlib.Path(file_name)
 lines7 = p.read_text(encoding='UTF-8' )
 lines7separated = lines7.splitlines()
